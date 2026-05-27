955012a900c8373e67bf77bffb2e47f1507acef2
prime-reset <clementine.signori@gmail.com>
2026-05-27 23:55:13 +0200

    Update layout.jsx

--- prime-v1-app/app/layout.jsx
+++ prime-v1-app/app/layout.jsx
@@ -8,13 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body>
-        <div className="prime-global-bg" />
-
-        <div className="prime-app">
-          {children}
-        </div>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
