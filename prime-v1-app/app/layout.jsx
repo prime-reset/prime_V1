2b20046c67af818e749758172885b7f75ddaacba
prime-reset <clementine.signori@gmail.com>
2026-08-02 23:33:55 +0200

    Update layout.jsx  provider

--- prime-v1-app/app/layout.jsx
+++ prime-v1-app/app/layout.jsx
@@ -8,7 +8,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body>{children}</body>
+      <body>
+  <PrimeToastProvider>
+    {children}
+  </PrimeToastProvider>
+</body>
     </html>
   );
 }
